6833ccd6cc0a7206c39a1c0c3738d0631f22deaf
Chí Hưởng <>
2023-10-27 23:19:56 +0700

    done

--- xuong-moc/src/App.js
+++ xuong-moc/src/App.js
@@ -11,24 +11,33 @@ import { Route, Routes } from "react-router-dom";
 import listRouter from "./router";
 import Footer from "./components/Footer";
 import "react-toastify/dist/ReactToastify.css";
+import ScrollToTop from "./Scroll";
 import { ToastContainer } from "react-toastify";
 // import "./js/main";
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        {listRouter.map((route, index) => {
-          const Page = route.component;
-          return <Route key={index} path={route.path} element={<Page />} />;
-        })}
-        {/* <Route path="/Products" element={<Products />}></Route>
+      <ScrollToTop>
+        <Routes>
+          {listRouter.map((route, index) => {
+            const ComponentSelect = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={<ComponentSelect />}
+              />
+            );
+          })}
+          {/* <Route path="/Products" element={<Products />}></Route>
         <Route path="/Contact" element={<Contact />}></Route>
         <Route path="/News" element={<News />}></Route>
         {/* components child */}
-        {/* <Route path="/ProductItem" element={<ProductItem />}></Route> */}{" "}
-        {/* */}
-      </Routes>
+          {/* <Route path="/ProductItem" element={<ProductItem />}></Route> */}{" "}
+          {/* */}
+        </Routes>
+      </ScrollToTop>
       <Footer />
       <ToastContainer />
     </>
